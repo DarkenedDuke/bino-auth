d722fe5c1f0b45e06723c45a05f28cb2fb24c58f
Dionizen Geisler Bino <152365462+DarkenedDuke@users.noreply.github.com>
2025-10-27 17:31:00 +0800

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -2,18 +2,16 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyA8BjZ6z4mWhQbLjhBZbHZZgCkwc2nizvc",
+  authDomain: "bino-auth-firebase.firebaseapp.com",
+  projectId: "bino-auth-firebase",
+  storageBucket: "bino-auth-firebase.firebasestorage.app",
+  messagingSenderId: "755005757558",
+  appId: "1:755005757558:web:4a4f9265065d3608f36757"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
